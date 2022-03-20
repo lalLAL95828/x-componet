445b4c93701bc64c6e024c03a3df948c49687df9
lallal_win10 <1445668063@qq.com>
2022-03-20 14:45:57 +0800

    ceshi

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -20,6 +20,8 @@ describe('test Button component', () => {
     const wrapper = render(<Button {...defaultProps}>Nice</Button>)
     const element = wrapper.getByText('Nice') as HTMLButtonElement
     expect(element).not.toBeInTheDocument()
+
+    
     expect(element.tagName).toEqual('BUTTON')
     expect(element).toHaveClass('btn btn-default')
     expect(element.disabled).toBeFalsy()
