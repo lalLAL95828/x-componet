d3f9ef59959b2649f242d87d079e13fa70a80c5d
lallal_win10 <1445668063@qq.com>
2022-03-20 21:03:53 +0800

    lal

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -19,9 +19,7 @@ describe('test Button component', () => {
   it('should render the correct default button', () => {
     const wrapper = render(<Button {...defaultProps}>Nice</Button>)
     const element = wrapper.getByText('Nice') as HTMLButtonElement
-    expect(element).not.toBeInTheDocument()
-
-    
+    expect(element).toBeInTheDocument()
     expect(element.tagName).toEqual('BUTTON')
     expect(element).toHaveClass('btn btn-default')
     expect(element.disabled).toBeFalsy()
